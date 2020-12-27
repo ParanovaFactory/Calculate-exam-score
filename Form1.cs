a75cf593d867b7e7f67c158ed8fd1f9b8f7601f9
Berkay <bkaraduman64@gmail.com>
2020-12-27 18:51:33 +0300

    Sınav Not Ortalaması Hesaplama

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınav_Notu_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            int s1, s2, prj;
+            double ort;
+            ad = textBox4.Text;
+            soyad = textBox5.Text;
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            prj = Convert.ToInt32(textBox3.Text);
+            ort = (s1 + s2 + prj) / 3;
+            listBox1.Items.Add(ad+ " " + soyad + " Ortalama: " +ort);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Sinav-Not-Hesaplama
+# Sınav Not Hesaplama
+# Sisteme girilen notları herhangi bir kat sayı çarpımına oğratmadan doğrudan hesaplama yapar sonrasında ad soyad bilgisi ile birlikte geçip kalma durumunu listbox'a yazar
